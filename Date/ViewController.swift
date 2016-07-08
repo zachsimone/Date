643c48665ae7eb485036a1f91042f4c17f4bc931
Zach Simone <Zach@zachs-macbook-pro.local>
2016-07-08 21:23:25 +1000

    Initial Commit

---
+++ Date/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Date
+//
+//  Created by Zach Simone on 8/7/16.
+//  Copyright © 2016 Zachary Simone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
